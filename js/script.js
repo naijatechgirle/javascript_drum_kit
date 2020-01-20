2d0df8e362cca274228df1656e5bf426fe7edb86
kelly <kellyeluagu@gmail.com>
2020-01-21 00:11:23 +0100

    Latest addition

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    audio.currentTime = 0;
     if(!audio) return;
+    audio.currentTime = 0;
     audio.play();
-    key.classList.add('playing');ll
+    key.classList.add('playing');
 };
 
 function removeTransition(e) {
